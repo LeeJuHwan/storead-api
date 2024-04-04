83021a6cc8b5f97d4d63dc499ae97ee1b0566be9
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-05 00:21:52 +0900

    feat: load secret variables use yaml file

---
+++ booklog/utils/load_yaml.py
@@ -0,0 +1,13 @@
+import yaml
+import os
+
+
+def load_yaml_file(path: str):
+    """
+    Load yaml file from project directory.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(path)
+
+    with open(path, "r") as f:
+        return yaml.safe_load(f).get("social")

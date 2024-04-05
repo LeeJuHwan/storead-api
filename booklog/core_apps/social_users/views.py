40a87360eccd39e5d47841f2c6e2e5c488702024
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-05 20:08:05 +0900

    feat: social login business logic

--- booklog/core_apps/social_users/views.py
+++ booklog/core_apps/social_users/views.py
@@ -1,29 +1,57 @@
+from typing import Optional
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+from .domain import SocialPlatform
+
 
 class BaseSocialLogin(APIView):
     permission_classes = (AllowAny,)
 
+    client_id: str = None
+    client_secret: str = None
+    redirect_uri: str = None
+    state: Optional[str] = None
+    platform: str = None
+
+    @property
+    def social_domain(self):
+        oauth_info = getattr(SocialPlatform, self.platform, None)
+
+        if not oauth_info:
+            raise ValueError(f"{self.platform} is not supported")
+
+        return oauth_info.info.to_dict()
+
+    @property
+    def auth(self):
+        return {
+            "platform": self.platform,
+            "fields": {attr: value for attr, value in self.social_domain.items() if value}}
+
     def get(self, request):
         request_dict = dict(request.META)
-        payload = {
+        print(f"auth: {self.auth}")
+
+        # TODO: 응답 값 수정
+        context = {
             "request": request_dict.get("REQUEST_METHOD"),
             "endpoint": request_dict.get("PATH_INFO"),
             "client_ip": request_dict.get("REMOTE_ADDR"),
             }
-        return Response(payload, status=status.HTTP_200_OK)
+
+        return Response(context, status=status.HTTP_200_OK)
 
 
 class GoogleLogin(BaseSocialLogin):
-    pass
+    platform = "google"
 
 
 class KakaoLogin(BaseSocialLogin):
-    pass
+    platform = "kakao"
 
 
 class GithubLogin(BaseSocialLogin):
-    pass
+    platform = "github"

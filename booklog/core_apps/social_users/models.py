f28cf80a8381a85f8ee507c28bbb1e980db174a3
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-07 00:41:59 +0900

    fix: social user fields edit enable

--- booklog/core_apps/social_users/models.py
+++ booklog/core_apps/social_users/models.py
@@ -12,12 +12,9 @@ class Admin(AbstractUser):
 
 class SocialUser(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
-    uuid = models.CharField(max_length=100, unique=True, editable=False,
-                            verbose_name=_("Social user identifier"))
-    name = models.CharField(max_length=50, editable=False,
-                            verbose_name=_("Social user name"))
-    provider = models.CharField(max_length=50, editable=False,
-                                verbose_name=_("Social provider"))
+    uuid = models.CharField(max_length=100, unique=True, verbose_name=_("Social user identifier"))
+    name = models.CharField(max_length=50, verbose_name=_("Social user name"))
+    provider = models.CharField(max_length=50, verbose_name=_("Social provider"))
     last_login = models.DateTimeField(verbose_name=_("last login"), auto_now=True)
     date_joined = models.DateTimeField(verbose_name=_("date joined"), auto_now_add=True)
 

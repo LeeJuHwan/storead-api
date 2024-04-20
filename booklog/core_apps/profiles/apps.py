baffb92ecca1f32d62ffee510759cdfb220c4210
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-20 18:53:30 +0900

    feat: create a profile when a user created

--- booklog/core_apps/profiles/apps.py
+++ booklog/core_apps/profiles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core_apps.profiles"
+
+    def ready(self):
+        from core_apps.profiles import signals
--- booklog/core_apps/profiles/signals.py
+++ booklog/core_apps/profiles/signals.py
@@ -4,13 +4,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from core_apps.profiles.models import Profile
-from core_apps.social_users.models import SocialUser
+from config.settings.base import AUTH_USER_MODEL
 
 logger = logging.getLogger(__name__)
 
 
-@receiver(post_save, sender=SocialUser)
+@receiver(post_save, sender=AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwagrs):
+    logger.info(f"created: {created}")
     if created:
+        logger.info(f"instance: {instance}")
         Profile.objects.create(user=instance)
         logger.info(f"[profile signal] {instance}'s profile has been created.")

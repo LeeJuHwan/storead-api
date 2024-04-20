ff9b5ce4e4899b3d34a59aa95d7bdd79b006f4bc
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-20 18:51:33 +0900

    fix: add custom command and custom manager

--- booklog/core_apps/social_users/admin.py
+++ booklog/core_apps/social_users/admin.py
@@ -2,19 +2,39 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.backends import BaseBackend
 
 from .forms import UserChangeForm, UserCreationForm, SocialUserCreationForm
-from .models import SocialUser
+from .models import Admin
 
 
 User = get_user_model()
+
+
+class AdminAuthBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            admin = Admin.objects.get(username=username)
+            if admin.check_password(password):
+                return admin
+        except Admin.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return Admin.objects.get(pk=user_id)
+        except Admin.DoesNotExist:
+            return None
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ["-date_joined"]
     form = UserChangeForm
     add_form = UserCreationForm
-    model = User
+    model = Admin
+
+    def delete_model(self, request, obj):
+        obj.delete()
 
     list_display = [
         "id",
@@ -55,29 +75,32 @@ class SocialAccount(admin.ModelAdmin):
     ordering = ["-date_joined"]
     form = UserChangeForm
     add_form = SocialUserCreationForm
-    model = SocialUser
+    model = User
+
+    def delete_model(self, request, obj):
+        obj.delete()
 
     list_display = [
         "pkid",
         "uuid",
         "provider",
-        "name",
+        "username",
         "last_login",
         "date_joined",
     ]
 
-    list_display_links = ["pkid", "name", "provider"]
+    list_display_links = ["pkid", "uuid", "username", "provider"]
 
     list_filter = [
-        "name",
+        "username",
         "uuid",
         "provider",
         "last_login",
         "date_joined",
     ]
 
-    search_fields = ["name", "provider"]
+    search_fields = ["username", "provider"]
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(SocialUser, SocialAccount)
+admin.site.register(Admin, UserAdmin)
+admin.site.register(User, SocialAccount)
--- booklog/core_apps/social_users/forms.py
+++ booklog/core_apps/social_users/forms.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 
-from .models import SocialUser
+from .models import Admin
 
 User = get_user_model()
 
 
 class UserChangeForm(admin_forms.UserChangeForm):
     class Meta(admin_forms.UserChangeForm.Meta):
-        model = User
+        model = Admin
 
 
 class UserCreationForm(admin_forms.UserCreationForm):
     class Meta(admin_forms.UserCreationForm.Meta):
-        model = User
+        model = Admin
         fields = ["username"]
 
     error_messages = {
@@ -23,9 +23,9 @@ class UserCreationForm(admin_forms.UserCreationForm):
 
 class SocialUserCreationForm(admin_forms.UserCreationForm):
     class Meta(admin_forms.UserCreationForm.Meta):
-        model = SocialUser
-        fields = ["uuid", "name", "provider"]
+        model = User
+        fields = ["uuid", "username", "provider"]
 
     error_messages = {
-        "name": "A user with this username already exists.",
+        "username": "A user with this username already exists.",
     }

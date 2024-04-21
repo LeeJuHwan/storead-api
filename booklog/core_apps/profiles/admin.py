f6618be32e07ed2363628df1c3a8f94c6f536564
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-21 14:46:36 +0900

    chore: admin site in delete foreignkey field

--- booklog/core_apps/profiles/admin.py
+++ booklog/core_apps/profiles/admin.py
@@ -3,14 +3,25 @@ from django.contrib import admin
 from .models import Profile
 
 
+@admin.action(description="선택된 프로필 삭제")
+def delete_profile_without_user(modeladmin, request, queryset):
+    for profile in queryset:
+        profile.delete()
+
+
 class ProfileAdmin(admin.ModelAdmin):
     list_display = [
         "pkid",
         "id",
+        "user_name",
         "user",
     ]
-    list_display_links = ["pkid", "id", "user"]
+    list_display_links = ["pkid", "id", "user", "user_name"]
     list_filter = ["id", "pkid"]
+    actions = [delete_profile_without_user]
+
+    def user_name(self, obj):
+        return obj.user.username
 
 
 admin.site.register(Profile, ProfileAdmin)

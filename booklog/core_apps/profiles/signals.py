7f76eace07e333ac4f6a76edcd11f36afd822f01
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-21 14:47:01 +0900

    chore: remove signal logging

--- booklog/core_apps/profiles/signals.py
+++ booklog/core_apps/profiles/signals.py
@@ -1,18 +1,11 @@
-import logging
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from core_apps.profiles.models import Profile
 from config.settings.base import AUTH_USER_MODEL
 
-logger = logging.getLogger(__name__)
-
 
 @receiver(post_save, sender=AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwagrs):
-    logger.info(f"created: {created}")
     if created:
-        logger.info(f"instance: {instance}")
         Profile.objects.create(user=instance)
-        logger.info(f"[profile signal] {instance}'s profile has been created.")

6c2b2638b63fe1240aac0b3df07aaa5d9aed36e5
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-04-21 14:47:33 +0900

    chore: exclude dj_rest_auth library and change admin site title name

--- booklog/config/urls.py
+++ booklog/config/urls.py
@@ -6,15 +6,12 @@ from django.conf import settings
 urlpatterns = [
     path(f"{settings.ADMIN_URL}", admin.site.urls),
     path("api/v1/auth/", include("core_apps.social_users.urls")),
-    path("api/v1/auth/", include("dj_rest_auth.urls")),
     path("api/v1/articles/", include("core_apps.articles.urls")),
     path("api/v1/books/", include("core_apps.books.urls")),
     path("api/v1/profiles/", include("core_apps.profiles.urls")),
 ]
 
 
-admin.site.site_header = "Booklog API Admin"
-
-admin.site.site_title = "Booklog API Admin Portal"
-
-admin.site.index_title = "Welcome to Booklog API Portal"
+admin.site.site_header = "Storead API Admin"
+admin.site.site_title = "Storead API Admin Portal"
+admin.site.index_title = "Welcome to Storead API Portal"

9ceb432a446819094a9c19e2cb280df948f4302a
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-05-05 23:25:00 +0900

    chore: modify build command for access environment settings file

--- booklog/manage.py
+++ booklog/manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    from config.settings.base import env
-
-    _env = env.str("DJANGO_ENV", default="dev")
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{_env}")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

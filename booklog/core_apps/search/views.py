70c142ca27bb639f812fe7d0cdb47eee35625bf6
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-05-28 00:23:18 +0900

    feat: elasticsearch api documentation

--- booklog/core_apps/search/views.py
+++ booklog/core_apps/search/views.py
@@ -6,8 +6,10 @@ from django_elasticsearch_dsl_drf.filter_backends import (
     SearchFilterBackend,
 )
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from drf_spectacular.utils import extend_schema
 from rest_framework import permissions
 
+from .docs import ordering_parameter, search_parameter
 from .documents import ArticleDocument
 from .serializers import ArticleElasticSearchSerializer
 
@@ -44,3 +46,11 @@ class ArticleElasticSearchView(DocumentViewSet):
         "created_at": "created_at",
     }
     ordering = ("-created_at",)
+
+    @extend_schema(
+        summary="게시글 검색 API",
+        tags=["검색"],
+        parameters=[search_parameter, ordering_parameter],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)

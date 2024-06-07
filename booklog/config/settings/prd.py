4696ab47732c45805c8f8769f2921aadf4629916
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-06-07 23:47:55 +0900

    chore: change rdbms mysqlclient to postgres

--- booklog/config/settings/prd.py
+++ booklog/config/settings/prd.py
@@ -3,10 +3,8 @@ from .base import env
 
 DEBUG = env.ENV.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api.storead.site"]
 
 SITE_NAME = "Storead"
-
-CORS_ALLOW_ALL_ORIGINS: True
 
 DATABASES = {"default": env.ENV.db("DATABASE_URL")}

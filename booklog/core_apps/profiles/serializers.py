15499c754054286816ad0916aab7ce02f34c960b
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-06-24 20:23:30 +0900

    update: add response user's profile id

--- booklog/core_apps/profiles/serializers.py
+++ booklog/core_apps/profiles/serializers.py
@@ -1,17 +1,20 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 
-from .exceptions import EmptyUserNameException
+from .exceptions import AlreadyUseUserNameError
 from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source="user.username")
     user_id = serializers.CharField(source="user.uuid")
+    profile_id = serializers.CharField(source="id")
     profile_photo = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
         fields = [
+            "profile_id",
             "user_id",
             "name",
             "profile_photo",
@@ -23,7 +26,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class UpdateProfileSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source="user.username")
+    name = serializers.CharField(source="user.username", required=False)
 
     class Meta:
         model = Profile
@@ -35,22 +38,25 @@ class UpdateProfileSerializer(serializers.ModelSerializer):
 
     # NOTE: 유저랑 관계형을 맺고 있기 때문에 upadte를 직접적으로 명시 해야함
     def update(self, instance, validated_data):
-        self.fields.pop("name")
         user_data = validated_data.pop("user", {})
         username = user_data.get("username")
 
-        if username:
-            instance.user.username = username
-            instance.user.save()
-        # NOTE: 유저가 자신의 이름을 설정하지 않은 경우
-        else:
-            raise EmptyUserNameException()
-
         for field in self.fields:
+            if field == "name":
+                continue
+
             input_data = validated_data.get(field, getattr(instance, field))
             setattr(instance, field, input_data)
-        instance.save()
 
+        # NOTE: 유저네임이 제공되었고, 기존 유저네임과 다른 경우에만 업데이트
+        if username and username != instance.user.username:
+            instance.user.username = username
+            try:
+                instance.user.save()
+            except IntegrityError:
+                raise AlreadyUseUserNameError()
+
+        instance.save()
         return instance
 
 
@@ -65,3 +71,8 @@ class FollowingSerializer(serializers.ModelSerializer):
             "profile_photo",
             "about_me",
         ]
+
+
+class FollowResponseSerializer(serializers.Serializer):
+    followers_count = serializers.IntegerField()
+    following = FollowingSerializer(many=True)

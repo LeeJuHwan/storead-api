3c74ea314efcb287f565e01229c3453b883cef79
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-07-26 22:41:24 +0900

    update: S3 access image file expire option disable

--- src/config/settings/storages.py
+++ src/config/settings/storages.py
@@ -20,3 +20,5 @@ if USE_S3:
     AWS_LOCATION = "media"
     MEDIA_URL = f"https://{AWS_S3_URL}/{AWS_LOCATION}/"
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+    AWS_QUERYSTRING_AUTH = False
